fcb944fb17f590f375e553139fed30eecca0b6cb
rew asd <fork213@126.com>
2023-04-23 08:47:03 +0800

    添加项目文件。

---
+++ Plugin_based/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Plugin_based
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
